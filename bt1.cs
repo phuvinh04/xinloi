97a2bdea1be6de0602c5e649b845760f674edbe7
phuvinh04 <131090806+phuvinh04@users.noreply.github.com>
2024-10-30 23:22:10 +0700

    Initial commit

---
+++ bt1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Bai1
+{
+    class Program
+    {
+
+         static void Main(string[] args)
+         {
+            Console.WriteLine("Hello World!");
+    }
+}
+}
